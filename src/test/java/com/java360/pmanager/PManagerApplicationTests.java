f168a15bd9b43f1012894993fa87028d347871ad
hendreo santos <hendreosantos1@outlook.com>
2026-04-01 21:59:01 -0300

    initial

---
+++ src/test/java/com/java360/pmanager/PManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java360.pmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
