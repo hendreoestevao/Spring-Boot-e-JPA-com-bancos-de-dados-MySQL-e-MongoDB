c16fd5b256d2c826748156e7f3ccde3a85686b1d
hendreo santos <hendreosantos1@outlook.com>
2026-04-14 22:10:51 -0300

    ajustando apis de membro

--- src/main/java/com/java360/pmanager/domain/applicationservice/MemberService.java
+++ src/main/java/com/java360/pmanager/domain/applicationservice/MemberService.java
@@ -1,11 +1,15 @@
 package com.java360.pmanager.domain.applicationservice;
 
 import com.java360.pmanager.domain.entity.Member;
+import com.java360.pmanager.domain.exception.DuplicateMemberException;
+import com.java360.pmanager.domain.exception.MemberNotFoundException;
 import com.java360.pmanager.domain.repository.MemberRepository;
 import com.java360.pmanager.infrastructure.dto.SaveMemberDataDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Objects;
 import java.util.UUID;
 
 @Service
@@ -15,6 +19,10 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     public Member createMember(SaveMemberDataDTO saveMemberData) {
+        if (existsMemberWithEmail(saveMemberData.getEmail(), null)) {
+            throw new DuplicateMemberException(saveMemberData.getEmail());
+        }
+
         Member member = Member.builder()
                 .name(saveMemberData.getName())
                 .email(saveMemberData.getEmail())
@@ -24,5 +32,36 @@ public class MemberService {
 
         memberRepository.save(member);
         return member;
+    }
+
+    public Member loadMemberById(String memberId) {
+        return memberRepository.findByIdAndDeleted(memberId, false).orElseThrow(
+                () -> new MemberNotFoundException(memberId)
+        );
+    }
+
+    @Transactional
+    public void deleteMember(String memberId) {
+        Member member = loadMemberById(memberId);
+        member.setDeleted(true);
+    }
+
+
+    @Transactional
+    public Member updateMember(String memberId, SaveMemberDataDTO saveMemberData) {
+        if (existsMemberWithEmail(saveMemberData.getEmail(), memberId)) {
+            throw new DuplicateMemberException(saveMemberData.getEmail());
+        }
+        Member member = loadMemberById(memberId);
+        member.setName(saveMemberData.getName());
+        member.setEmail(saveMemberData.getEmail());
+        return member;
+    }
+
+    private boolean existsMemberWithEmail(String email, String idToExclude) {
+        return memberRepository.findByEmailAndDeleted(email, false).filter(
+                        member -> !Objects.equals(member.getId(), idToExclude))
+                .isPresent();
+
     }
 }
--- src/main/java/com/java360/pmanager/infrastructure/controller/MemberRestResource.java
+++ src/main/java/com/java360/pmanager/infrastructure/controller/MemberRestResource.java
@@ -7,7 +7,11 @@ import com.java360.pmanager.infrastructure.dto.SaveMemberDataDTO;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +33,23 @@ public class MemberRestResource {
         Member member = memberService.createMember(saveMemberData);
         return ResponseEntity.created(URI.create(PATH_MEMBERS + "/" + member.getId()))
                 .body(MemberDTO.create(member));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MemberDTO> loadMemberById(@PathVariable("id") String memberId) {
+        Member member = memberService.loadMemberById(memberId);
+        return ResponseEntity.ok(MemberDTO.create(member));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMember(@PathVariable("id") String memberId) {
+        memberService.deleteMember(memberId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MemberDTO> updateMember(@PathVariable("id") String memberId, @RequestBody @Valid SaveMemberDataDTO saveMemberData) {
+        Member member = memberService.updateMember(memberId, saveMemberData);
+        return ResponseEntity.ok(MemberDTO.create(member));
     }
 }
